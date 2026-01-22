5db988809dfe2f3d1bd306e8c117e1cf64adea2c
Ori <ori.rowan@gmail.com>
2026-01-22 14:43:30 +0100

    rodent movement change

--- src/gameplay/rodent.lua
+++ src/gameplay/rodent.lua
@@ -7,6 +7,12 @@ function Rodent:new(tbl)
     setmetatable(tbl, self)
     
     assert(tbl.scene)
+
+    tbl.x=60
+    tbl.y=60
+
+    tbl.min_move_offset = 30
+    tbl.border = 8
     
     tbl:move()
 
@@ -14,8 +20,21 @@ function Rodent:new(tbl)
 end
 
 function Rodent:move()
-    self.x=flr(rnd(128))
-    self.y=flr(rnd(128))
+
+    local last_pos = {x=self.x, y=self.y}
+    local new_pos = {x=self:get_random_pos(), y=self:get_random_pos()}
+    
+    while get_vector_length(get_vector_from_points(last_pos, new_pos)) < 30 do
+        new_pos.x=self:get_random_pos()
+        new_pos.y=self:get_random_pos()
+    end
+    
+    -- log('last pos: '..last_pos.x..","..last_pos.y)
+    -- log('new pos: '..new_pos.x..","..new_pos.y)
+    -- log('distance from last pos '..tostring(get_vector_length(get_vector_from_points(last_pos, new_pos))))
+
+    self.x=new_pos.x
+    self.y=new_pos.y
 end
 
 function Rodent:squeek(dist)
@@ -24,4 +43,8 @@ function Rodent:squeek(dist)
     log("Squek vol: "..volume)
     change_sfx_volume(0, volume)
     sfx(0)    
+end
+
+function Rodent:get_random_pos()
+    return self.border+flr(rnd(129-self.border))
 end
